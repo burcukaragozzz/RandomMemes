57a8c527ab4788e8acf65f20294471fe89c18772
ytu-burcu-karagoz <burcu.karagoz@ytu-burcu-karagozs-MacBook-Air.local>
2019-08-28 14:25:51 +0300

    Random Images Generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      textTop: 'Random',
+      textBottom: 'Meme',
+      size: 24,
+      randomImg: '',
+      allImgs: [],
+    };
+  }
+
+  handleClick = () => {
+    const { allImgs } = this.state;
+    let randomNumber = Math.floor(
+      Math.random() * allImgs.length
+    );
+
+    this.setState({ randomImg: allImgs[randomNumber].url });
+  };
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(images => images.json())
+      .then(response => {
+        const { memes } = response.data;
+        this.setState({ 
+          allImgs: memes, 
+        }, () => {
+          this.handleClick();
+        });
+      });
+  }
+
+  handleChange(event, type) {
+    const { value } = event.target;
+
+    this.setState({
+      [type]: value,
+    })
+  }
+
+  render() {
+    const { randomImg, size, textTop, textBottom } = this.state;
+    
+    return(
+      <>
+        <div className="form">
+          <input className="inputText" type="text" value={textTop} placeholder="Enter the text here" onChange={e => this.handleChange(e, 'textTop')} />
+          <input className="inputText" type="text" value={textBottom} placeholder="Enter the text here" onChange={e => this.handleChange(e, 'textBottom')} />
+          <input className="inputText" type="number" value={size} onChange={e => this.handleChange(e, 'size')} />
+          <button className="generate" onClick={this.handleClick}>Generate!</button>
+        </div>
+        <div className="result" style={ { fontSize: `${ size }px` } }>
+          <figure className="image-wrapper"> 
+            <img class="randomImage" src={randomImg} alt="backgroundImage"/>
+          </figure>
+          <h1 className="text text_top"> {textTop} </h1>
+          <h1 className="text text_bottom"> {textBottom}</h1>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
+
+
---
+++ src/image.js
@@ -0,0 +1,8 @@
+const urls = [
+    "https://images.unsplash.com/photo-1566725130422-095170e539de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1562243311-dd5bac34818f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=335&q=80",
+    "https://images.unsplash.com/photo-1560943026-1c6dd85171f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1547760039-542180757c90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+  ];
+
+  export default urls;
